99de1f503cdde9bfed08002e93b8564f8af718e0
bezel <dawids14@onet.pl>
2018-10-18 11:54:28 +0200

    Add some additives

---
+++ SportsStore/Models/SeedData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            if(!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kajak",
+                        Description = "Łódka przeznaczona dla jednej osoby",
+                        Category = "Sporty wodne",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kamizelka ratunkowa",
+                        Description = "Chroni i dodaje uroku",
+                        Category = "Sporty wodne",
+                        Price = 48.95m
+                    },
+                    new Product
+                    {
+                        Name = "Piłka",
+                        Description = "Zatwierdzone przez FIFA rozmiar i waga",
+                        Category = "Pilka nożna",
+                        Price = 19.50m
+                    },
+                    new Product
+                    {
+                        Name = "Flagi narożne",
+                        Description = "Nadzadzą twojemu boisku profesjonalny wyglad",
+                        Category = "Piłka nożna",
+                        Price = 34.95m
+                    },
+                    new Product
+                    {
+                        Name = "Stadiom",
+                        Description = "Skladany stadion na 35 000 osób",
+                        Category = "Piłka nożna",
+                        Price = 79500
+                    },
+                    new Product
+                    {
+                        Name = "Czapka",
+                        Description = "Zwiększa efektywność mózgu",
+                        Category = "Szachy",
+                        Price = 16
+                    }
+         
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SportsStore/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuraion) =>
+            Configuration = configuraion;
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>();/*(options =>*/
+            //options.UseMySql(Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+                app.UseStaticFiles();
+                app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: null,
+                        template: "{category}/Strona{productPage:int}",
+                        defaults: new { controller = "Product", action = "List" });
+                    routes.MapRoute(
+                        name: null,
+                        template: "Strona{productPage:int}",
+                        defaults: new { controller = "Product", action = "List", productPage = 1 });
+                    routes.MapRoute(
+                        name: null,
+                        template: "{category}",
+                        defaults: new { controller = "Product", action = "List", productPage = 1 });
+                    routes.MapRoute(
+                        name: null,
+                        template: "",
+                        defaults: new { controller = "Product", action = "List", productPage = 1 });
+                    routes.MapRoute(name: null, template: "{controller=Product}/{action=List}/{id?}");
+                });
+                SeedData.EnsurePopulated();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
