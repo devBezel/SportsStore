2d5c367bb36b48fe44f7a8ec39b130a389a0d8f1
bezel <dawids14@onet.pl>
2018-10-23 12:28:23 +0200

    Add various improvements

---
+++ SportsStore/Models/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -25,6 +25,8 @@ namespace SportsStore
             //options.UseMySql(Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +37,7 @@ namespace SportsStore
                 app.UseDeveloperExceptionPage();
                 app.UseStatusCodePages();
                 app.UseStaticFiles();
+                app.UseSession();
                 app.UseMvc(routes =>
                 {
                     routes.MapRoute(
