d16b2cc87d93bbd573ee029e9d94385276b1bd16
bezel <dawids14@onet.pl>
2018-10-26 11:47:07 +0200

    Add various improvements

---
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using SportsStore.Models;
+using SportsStore.Controllers;
+using Moq;
+using Xunit;
+using System.Linq;
+
+namespace SportsStore.Tests
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void CanSaveValidChanges()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+            Mock<ITempDataDictionary> tempData = new Mock<ITempDataDictionary>();
+            AdminController target = new AdminController(mock.Object)
+            {
+                TempData = tempData.Object
+            };
+
+            Product product = new Product { Name = "Test" };
+
+            IActionResult result = target.Edit(product);
+
+            mock.Verify(m => m.SaveProduct(product));
+            Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", (result as RedirectToActionResult).ActionName);
+        }
+
+        [Fact]
+        public void CannotSaveInvalidChanges()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            AdminController target = new AdminController(mock.Object);
+
+            Product product = new Product { Name = "Test" };
+            target.ModelState.AddModelError("error", "error");
+
+            IActionResult result = target.Edit(product);
+            mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never());
+            Assert.IsType<ViewResult>(result);
+        }
+        [Fact]
+        public void IndexContainsAllProducts()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            }.AsQueryable<Product>());
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product[] result = GetViewData<IEnumerable<Product>>(target.Index())?.ToArray();
+
+            Assert.Equal(3, result.Length);
+            Assert.Equal("P1", result[0].Name);
+            Assert.Equal("P2", result[1].Name);
+            Assert.Equal("P3", result[2].Name);
+        }
+
+        [Fact]
+        public void CanEditProduct()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            }.AsQueryable<Product>());
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product p1 = GetViewData<Product>(target.Edit(1));
+            Product p2 = GetViewData<Product>(target.Edit(2));
+            Product p3 = GetViewData<Product>(target.Edit(3));
+
+            Assert.Equal(1, p1.ProductID);
+            Assert.Equal(2, p2.ProductID);
+            Assert.Equal(3, p3.ProductID);
+        }
+
+        [Fact]
+        public void CannotaEditNonexistentProduct()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1"},
+                new Product { ProductID = 2, Name = "P2"},
+                new Product { ProductID = 3, Name = "P3"},
+            }.AsQueryable<Product>);
+
+            AdminController target = new AdminController(mock.Object);
+
+            Product result = GetViewData<Product>(target.Edit(4));
+
+            Assert.Null(result);
+        }
+
+        private T GetViewData<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+    }
+}
